d94be41ae1f6eb169ed52f1504c7d5d843a42737
holmityd <nbayramov100@gmail.com>
2025-05-02 18:38:33 +0400

    build: update base path to /compare-modpacks/
    Update the base path in vite.config.ts and related files to /compare-modpacks/ to ensure correct routing and resource loading for the application. This change affects the service worker, manifest, and asset paths.

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/compare-modpacks/sw.js', { scope: '/compare-modpacks/' })})}
